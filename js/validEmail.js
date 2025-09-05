d7f3448e2ddc5f76642267b8089470057ccd2a2b
POAPCA02LC0520 <SSDD@POAPCA02LC0520.duoc.acad>
2025-09-04 20:35:11 -0400

    Listo XD

--- js/validEmail.js
+++ js/validEmail.js
@@ -1,3 +1,5 @@
+const isValid = true;
+
 function validEmailFormat(email) {
     const validDomain = "@duocuc.cl";
     return email.endsWith(validDomain);
@@ -6,6 +8,14 @@ function validEmailFormat(email) {
 function validateForm() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const username = document.getElementById("username");
+    const cellphone = document.getElementById("cellphone");
+    const categoria = document.getElementById("categoria");
+
+    if (username.value === "" || email.value === "" || password.value === "") {
+        alert("Por favor, completa todos los campos.");
+    return isValid = false;
+    }
 
     if (!validEmailFormat(email)) {
         alert("Por favor, ingresa un correo electrónico válido.");
@@ -16,6 +26,15 @@ function validateForm() {
         alert("La contraseña debe tener al menos 10 caracteres.");
         return false;
     }
+    
+
+    if (isValid) {
+        alert(`Registro completado satisfactoriamente \n Usuario: ${username.value}, email: ${email}, Contra: ${password}, Celu: ${cellphone.value}, categoria: ${categoria.value}`);
+    } else {
+        alert("Por favor completar el registro")
+    }
 
     return true;
 }
+
+
